36182a5f6ec826821104ec7ef7ec91a46040e158
Wiamoh <wiamenginer123@gmail.com>
2023-09-19 07:15:52 -0700

    files

--- tokenize.c
+++ tokenize.c
@@ -1,93 +1,41 @@
 #include "shell.h"
-
 /**
- * **wiam_strtow - splits a string into words. Repeat delimiters are ignored
- * @wiam_str: the input string
- * @wiam_d: the delimiter string
- * Return: a pointer to an array of strings, or NULL on failure
+ * tokenize - this function separate the string using a designed delimiter
+ * @data: a pointer to the program's data
+ * Return: an array of the different parts of the string
  */
-
-char **wiam_strtow(char *wiam_str, char *wiam_d)
+void tokenize(data_of_program *data)
 {
- int wiam_i, wiam_j, wiam_k, wiam_m, wiam_numwords = 0;
- char **wiam_s;
+	char *delimiter = " \t";
+	int i, j, counter = 2, length;
 
- if (wiam_str == NULL || wiam_str[0] == 0)
-  return (NULL);
- if (!wiam_d)
-  wiam_d = " ";
- for (wiam_i = 0; wiam_str[wiam_i] != '\0'; wiam_i++)
-  if (!is_delim(wiam_str[wiam_i], wiam_d) && (is_delim(wiam_str[wiam_i + 1], wiam_d) || !wiam_str[wiam_i + 1]))
-   wiam_numwords++;
-if (wiam_numwords == 0)
-  return (NULL);
- wiam_s = malloc((1 + wiam_numwords) * sizeof(char *));
- if (!wiam_s)
-  return (NULL);
- for (wiam_i = 0, wiam_j = 0; wiam_j < wiam_numwords; wiam_j++)
- {
-  while (is_delim(wiam_str[wiam_i], wiam_d))
-   wiam_i++;
-  wiam_k = 0;
-  while (!is_delim(wiam_str[wiam_i + wiam_k], wiam_d) && wiam_str[wiam_i + wiam_k])
-   wiam_k++;
-  wiam_s[wiam_j] = malloc((wiam_k + 1) * sizeof(char));
-  if (!wiam_s[wiam_j])
-  {
-   for (wiam_k = 0; wiam_k < wiam_j; wiam_k++)
-    free(wiam_s[wiam_k]);
-   free(wiam_s);
-   return (NULL);
-  }
-  for (wiam_m = 0; wiam_m < wiam_k; wiam_m++)
-   wiam_s[wiam_j][wiam_m] = wiam_str[wiam_i++];
-  wiam_s[wiam_j][wiam_m] = 0;
- }
- wiam_s[wiam_j] = NULL;
- return (wiam_s);
+	length = str_length(data->input_line);
+	if (length)
+	{
+		if (data->input_line[length - 1] == '\n')
+			data->input_line[length - 1] = '\0';
+	}
+
+	for (i = 0; data->input_line[i]; i++)
+	{
+		for (j = 0; delimiter[j]; j++)
+		{
+			if (data->input_line[i] == delimiter[j])
+				counter++;
+		}
+	}
+
+	data->tokens = malloc(counter * sizeof(char *));
+	if (data->tokens == NULL)
+	{
+		perror(data->program_name);
+		exit(errno);
+	}
+	i = 0;
+	data->tokens[i] = str_duplicate(_strtok(data->input_line, delimiter));
+	data->command_name = str_duplicate(data->tokens[0]);
+	while (data->tokens[i++])
+	{
+		data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
+	}
 }
-/**
- * **wiam_strtow2 - splits a string into words
- * @wiam_str: the input string
- * @wiam_d: the delimiter
- * Return: a pointer to an array of strings, or NULL on failure
- */
-char **wiam_strtow2(char *wiam_str, char wiam_d)
-{
- int wiam_i, wiam_j, wiam_k, wiam_m, wiam_numwords = 0;
- char **wiam_s;
-
- if (wiam_str == NULL || wiam_str[0] == 0)
-  return (NULL);
- for (wiam_i = 0; wiam_str[wiam_i] != '\0'; wiam_i++)
-  if ((wiam_str[wiam_i] != wiam_d && wiam_str[wiam_i + 1] == wiam_d) ||
-      (wiam_str[wiam_i] != wiam_d && !wiam_str[wiam_i + 1]) || wiam_str[wiam_i + 1] == wiam_d)
-   wiam_numwords++;
- if (wiam_numwords == 0)
-  return (NULL);
- wiam_s = malloc((1 + wiam_numwords) * sizeof(char *));
- if (!wiam_s)
-  return (NULL);
- for (wiam_i = 0, wiam_j = 0; wiam_j < wiam_numwords; wiam_j++)
- {
-  while (wiam_str[wiam_i] == wiam_d && wiam_str[wiam_i] != wiam_d)
-   wiam_i++;
-  wiam_k = 0;
-  while (wiam_str[wiam_i + wiam_k] != wiam_d && wiam_str[wiam_i + wiam_k] && wiam_str[wiam_i + wiam_k] != wiam_d)
-   wiam_k++;
-  wiam_s[wiam_j] = malloc((wiam_k + 1) * sizeof(char));
-  if (!wiam_s[wiam_j])
-  {
-   for (wiam_k = 0; wiam_k < wiam_j; wiam_k++)
-    free(wiam_s[wiam_k]);
-   free(wiam_s);
-   return (NULL);
-  }
-  for (wiam_m = 0; wiam_m < wiam_k; wiam_m++)
-   wiam_s[wiam_j][wiam_m] = wiam_str[wiam_i++];
-  wiam_s[wiam_j][wiam_m] = 0;
- }
- wiam_s[wiam_j] = NULL;
- return (wiam_s);
-}
-
